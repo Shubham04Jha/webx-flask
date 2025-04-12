765cda1af72e70c2931f18f38f3100cfc41c2df5
Shubham04Jha <shubhamjha70491@gmail.com>
2025-04-12 17:17:07 +0530

    completed portfolio

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\91773\Desktop\SEM 6\WEB X\Portfolio\venv
+set VIRTUAL_ENV=C:\Users\Shubham\Documents\webx\shubham\WEBX-portfolio-website\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
